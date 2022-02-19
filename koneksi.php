30435baa0a30802cf17ad035ef4929a8a42777aa
siwa <siwarjsa@gmail.com>
2022-02-19 13:43:16 +0800

    update

--- koneksi.php
+++ koneksi.php
@@ -3,7 +3,6 @@ session_start();
 $host = 'localhost'; // host
 $username = 'root'; // username
 $password = ''; // password
-// $database = 'db_spp'; // nama database
 $database = 'db_spp_ardita'; // nama database
 $port = null; // port
 $socket = null; // socket
