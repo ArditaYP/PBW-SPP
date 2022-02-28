33d5fe3210fea58d4b1f2d4f130d638cda6fff77
ArditaYP <arditapinatih@gmail.com>
2022-02-28 15:25:39 +0800

    update

--- pembayaraan/prosespembayaraan.php
+++ pembayaraan/prosespembayaraan.php
@@ -4,14 +4,13 @@ session_start();
 $id_petugas = $_SESSION['id_ petugas'];
 $nisn = $_POST['nisn'];
 $tgl = $_POST['tgl'];
-$bulan = date ("F");
+$bulan = date("F");
 $tahun = date("Y");
 $id_spp = $_POST['id spp'];
 $bayar = $_POST['nominal'];
 
-include ("../koneksi.php");
 $query = "INSERT INTO pembayaran (id_petugas, nisn, tgl_bayar, bulan_ dibayar,tahun_dibayar, id_spp, jumlah_bayar)
     VALUES ('$id_petugas', '$nisn', '$tgl', '$bulan', '$tahun', '$id_spp', '$bayar')";
 
 $result = mysqli_query($conn, $query)
-        or die (mysqli_error ($conn));
+    or die(mysqli_error($conn));
