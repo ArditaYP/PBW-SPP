f12d230c82eca94186e2c859dd88bf83c6f75f53
siwa <siwarjsa@gmail.com>
2022-02-28 13:42:01 +0800

    update

--- templates/script.js
+++ templates/script.js
@@ -1,7 +1,6 @@
 const body = document.querySelector('body'),
     sidebar = body.querySelector('nav'),
     toggle = body.querySelector(".toggle"),
-    searchBtn = body.querySelector(".search-box"),
     modeSwitch = body.querySelector(".toggle-switch"),
     modeText = body.querySelector(".mode-text");
 
@@ -9,16 +8,11 @@ if (toggle != null) {
     toggle.addEventListener("click", () => {
         sidebar.classList.toggle("close");
     })
-
-    searchBtn.addEventListener("click", () => {
-        sidebar.classList.remove("close");
-    })
 }
 
 
 const angkatan = document.querySelector('#angkatan');
 const pembayaran = document.querySelector('#pembayaran');
 angkatan.addEventListener('change', (e) => {
-    console.log(e.value)
-    pembayaran.innerHTML = e.value;
+    pembayaran.innerHTML = e.target.value;
 });
